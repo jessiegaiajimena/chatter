88b5cafa28781ed6042ba2780141d949c066949a
Jiawei Yan <jy3021@ic.ac.uk>
2022-08-12 18:02:00 +0100

    push

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,60 @@
 import 'package:chatter/app.dart';
-import 'package:chatter/firebase_options.dart';
-import 'package:chatter/screens/screens.dart';
 import 'package:chatter/theme.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:stream_chat_flutter_core/stream_chat_flutter_core.dart';
 
 void main() async {
-  //WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  //final client = StreamChatClient(streamKey);
+  final client = StreamChatClient(streamKey);
 
+  await client.connectUser(User(id: 'tutorial-flutter'));
+  final channel = client.channel('messaging', id: 'flutterdevs');
+  channel.watch();
   runApp(
     MyApp(
-
+      client: client,
+      channel: channel,
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({
+    Key? key,
+    required this.client}) : super(key: key);
+    final StreamChatClient client;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light(),
+      darkTheme: AppTheme.dark(),
       themeMode: ThemeMode.dark,
       title: 'Chatter',
-      builder: (context, child) {
-        return ChatScreen(
-          
+      builder: (context, child){
+        return StreamChatCore(
+          client: client,
+          child: child!,
         );
       },
-      home: const SplashScreen(),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class ChannelPage extends StatelessWidget {
+  const ChannelPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children:[
+          Expanded(child: MessageListView(),)
+        ]
+      ),
+      MessageInput(),
     );
   }
 }
