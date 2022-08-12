27e9435f1c0b1ba0d26b40d9f328d573b7bc18c5
Jiawei Yan <jy3021@ic.ac.uk>
2022-08-12 14:28:38 +0100

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -7,41 +7,29 @@ import 'package:flutter/material.dart';
 import 'package:stream_chat_flutter_core/stream_chat_flutter_core.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  //WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  final client = StreamChatClient(streamKey);
+  //final client = StreamChatClient(streamKey);
 
   runApp(
     MyApp(
-      client: client,
-      appTheme: AppTheme(),
+
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({
-    Key? key,
-    required this.client,
-    required this.appTheme,
-  }) : super(key: key);
-
-  final StreamChatClient client;
-  final AppTheme appTheme;
-
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: appTheme.light,
-      darkTheme: appTheme.dark,
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.dark,
       title: 'Chatter',
       builder: (context, child) {
-        return StreamChatCore(
-          client: client,
-          child: child!,
+        return ChatScreen(
+          
         );
       },
       home: const SplashScreen(),
